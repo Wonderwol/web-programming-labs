a6e00bd88ece6873cb87ae7da3354d1bb8e704e5
Никита Темергалеев <mr.wonderwol@mail.ru>
2023-05-09 22:35:32 +0700

    Курс доллара

--- script7.js
+++ script7.js
@@ -7,3 +7,25 @@ function addBlocks() {
         blocks.appendChild(block);
     }
 }
+
+function showExchange() {
+    let exchange = [70.5174, 70.1217, 70.0414, 70.3847, 70.5991,
+                    70.8924, 71.5763, 72.8949, 72.7923, 73.6307];
+    let table = document.getElementById('dollar-exchange');
+    table.style.display = 'inline-block';
+    for (let i = 0; i < exchange.length; i++) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.innerHTML = exchange[i];
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
+
+    let sum = 0;
+    for (let i = 0; i < exchange.length; i++) {
+        sum = sum + exchange[i];
+    }
+
+    let average = sum / exchange.length;
+    document.getElementById('detail').innerHTML = 'Среднее значение: ' + average;
+}
