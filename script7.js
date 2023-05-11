c14b651f3eeb2cd647188060dcc7ac8795ca77f6
Темергалеев Никита <Mr.wonderwol@mail.ru>
2023-05-11 23:02:44 +0700

    Полупрозрачные блоки

--- script7.js
+++ script7.js
@@ -1,31 +1,59 @@
+let flag1 = true;
 function addBlocks() {
-    let blocks = document.getElementById('blocks');
-    for (let i = 0; i < 10; i++) {
-        let block = document.createElement('div');
-        block.innerHTML = 'Блок №' + (i + 1) + ': ' + (2 * (i + 1) ** 2);
+    if (flag1 == true) {
+        let blocks = document.getElementById('blocks');
+        for (let i = 0; i < 10; i++) {
+            let block = document.createElement('div');
+            block.innerHTML = 'Блок №' + (i + 1) + ': ' + (2 * (i + 1) ** 2);
+            block.style.borderColor = 'white';
+            block.style.width = '120px';
+            block.style.borderStyle = 'solid';
+            block.style.margin = '10px';
+            blocks.appendChild(block);
+        }
+    }
+    flag1 = false;
+}
 
-        blocks.appendChild(block);
+let flag2 = true;
+function showExchange() {
+    if (flag2 == true) {
+        let exchange = [70.5174, 70.1217, 70.0414, 70.3847, 70.5991,
+                        70.8924, 71.5763, 72.8949, 72.7923, 73.6307];
+        let table = document.getElementById('dollar-exchange');
+        table.style.display = 'inline-block';
+        for (let i = 0; i < exchange.length; i++) {
+            let tr = document.createElement('tr');
+            let td = document.createElement('td');
+            td.innerHTML = exchange[i];
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+
+        let sum = 0;
+        for (let i = 0; i < exchange.length; i++) {
+            sum = sum + exchange[i];
+        }
+
+        let average = sum / exchange.length;
+        document.getElementById('detail').innerHTML = 'Среднее значение: ' + average;
+        flag2 = false;
     }
 }
 
-function showExchange() {
-    let exchange = [70.5174, 70.1217, 70.0414, 70.3847, 70.5991,
-                    70.8924, 71.5763, 72.8949, 72.7923, 73.6307];
-    let table = document.getElementById('dollar-exchange');
-    table.style.display = 'inline-block';
-    for (let i = 0; i < exchange.length; i++) {
-        let tr = document.createElement('tr');
-        let td = document.createElement('td');
-        td.innerHTML = exchange[i];
-        tr.appendChild(td);
-        table.appendChild(tr);
+let flag3 = true;
+function addTransparentBlocks() {
+    if (flag3 == true) {
+        let tBlocks = document.getElementById('transparent-blocks');
+        for (let i = 0; i < 10; i++) {
+            let tBlock = document.createElement('div');
+            tBlock.style.borderColor = 'white';
+            tBlock.style.width = '20px';
+            tBlock.style.borderStyle = 'solid';
+            tBlock.style.margin = '10px';
+            tBlock.style.opacity = i / 10;
+            tBlocks.appendChild(tBlock);
+        }
     }
-
-    let sum = 0;
-    for (let i = 0; i < exchange.length; i++) {
-        sum = sum + exchange[i];
-    }
-
-    let average = sum / exchange.length;
-    document.getElementById('detail').innerHTML = 'Среднее значение: ' + average;
+    flag3 = false;
 }
