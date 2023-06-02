9396433e5f5e3fa49855f693c2a295e4c6dd0748
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-06-02 20:33:55 +0700

    Время

--- script8.js
+++ script8.js
@@ -1,7 +1,22 @@
 function showDate() {
-    let out = document.getElementById('current-date');
-    let today = new Date();
-    out.innerHTML = today.toLocaleString('ru-Ru');
+    let out1= document.getElementById('current-date1');
+    let out2= document.getElementById('current-date2');
+    let out3= document.getElementById('current-date3');
+    let out4= document.getElementById('current-date4');
+    let out5= document.getElementById('current-date5');
+    let out6= document.getElementById('current-date6');
+    let today1 = new Date();
+    let today2 = new Date();
+    let today3 = new Date();
+    let today4 = new Date();
+    let today5 = new Date();
+    let today6 = new Date();
+    out1.innerHTML = 'Дата и время для русской локали: ' + today1.toLocaleString('ru-Ru');
+    out2.innerHTML = 'Дата и время для канадской локали: ' + today2.toLocaleString('en-Ca');
+    out3.innerHTML = 'Дата и время для немецкой локали: ' + today3.toLocaleString('de-DE');
+    out4.innerHTML = 'Дата и время для испанской локали: ' + today4.toLocaleString('es-ES');
+    out5.innerHTML = 'Дата и время для арабской локали: ' + today5.toLocaleString('ar-AE');
+    out6.innerHTML = 'Дата и время для татарской локали: ' + today6.toLocaleString('tt-Ru');
 }
 
 function showDaysCount() {
@@ -22,3 +37,10 @@ function clearInput() {
     inputDate.value = '';
     showCount.innerHTML = '';
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-Ru');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 0.1)
