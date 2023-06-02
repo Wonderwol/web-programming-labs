e497ed57ae9d096817afb2ba4d38c596d8964837
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-06-02 20:49:26 +0700

    final

--- script8.js
+++ script8.js
@@ -5,18 +5,13 @@ function showDate() {
     let out4= document.getElementById('current-date4');
     let out5= document.getElementById('current-date5');
     let out6= document.getElementById('current-date6');
-    let today1 = new Date();
-    let today2 = new Date();
-    let today3 = new Date();
-    let today4 = new Date();
-    let today5 = new Date();
-    let today6 = new Date();
-    out1.innerHTML = 'Дата и время для русской локали: ' + today1.toLocaleString('ru-Ru');
-    out2.innerHTML = 'Дата и время для канадской локали: ' + today2.toLocaleString('en-Ca');
-    out3.innerHTML = 'Дата и время для немецкой локали: ' + today3.toLocaleString('de-DE');
-    out4.innerHTML = 'Дата и время для испанской локали: ' + today4.toLocaleString('es-ES');
-    out5.innerHTML = 'Дата и время для арабской локали: ' + today5.toLocaleString('ar-AE');
-    out6.innerHTML = 'Дата и время для татарской локали: ' + today6.toLocaleString('tt-Ru');
+    let today = new Date();
+    out1.innerHTML = 'Дата и время для русской локали: ' + today.toLocaleString('ru-Ru');
+    out2.innerHTML = 'Дата и время для канадской локали: ' + today.toLocaleString('en-Ca');
+    out3.innerHTML = 'Дата и время для немецкой локали: ' + today.toLocaleString('de-DE');
+    out4.innerHTML = 'Дата и время для испанской локали: ' + today.toLocaleString('es-ES');
+    out5.innerHTML = 'Дата и время для арабской локали: ' + today.toLocaleString('ar-AE');
+    out6.innerHTML = 'Дата и время для татарской локали: ' + today.toLocaleString('tt-Ru');
 }
 
 function showDaysCount() {
