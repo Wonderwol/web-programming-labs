cea6e339c63ed9e779a09e598b3aabee3d2e288b
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-06-02 19:44:05 +0700

    Количество дней с даты рождения

--- script8.js
+++ script8.js
@@ -3,3 +3,22 @@ function showDate() {
     let today = new Date();
     out.innerHTML = today.toLocaleString('ru-Ru');
 }
+
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let showCount = document.getElementById('showCount');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    showCount.style.color = 'white';
+    showCount.innerHTML ='Количество дней с даты рождения: ' + daysCount;
+
+}
+
+function clearInput() {
+    let inputDate = document.querySelector('input[type=date]');
+    let showCount = document.getElementById('showCount');
+    inputDate.value = '';
+    showCount.innerHTML = '';
+}
